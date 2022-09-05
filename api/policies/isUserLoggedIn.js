8b323b3094bc372683d95180671c2e4b13617377
Ali Gamal <ali.gamal@beyond-solution.com>
2022-09-05 21:53:38 +0200

    NF: create middleware for login user

---
+++ api/policies/isUserLoggedIn.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+const USER_JWT_SECRET_KEY =
+  require("../../config/env/local").USER_JWT_SECRET_KEY;
+  
+module.exports = async function (req, res, proceed) {
+
+    const header = req.header('Authorization');
+        
+    if (!header) {
+      throw 'Authorization: Bearer <token> header missing';
+      
+    }
+
+    const parts = header.split(' ');
+    if (parts.length !== 2 || parts[0] !== 'Bearer') {
+      throw 'Authorization: Bearer <token> header invalid' ;
+    }
+
+    const token = parts[1];
+
+    let jwtPayload = jwt.decode(token);
+
+    if (!jwtPayload) {
+      throw 'Invalid JWT payload';
+    }
+
+    if(jwtPayload['exp'] < Date.now()/1000){
+        throw 'Expired Token';
+    }
+
+    try {
+    
+      jwtPayload = jwt.verify(token, USER_JWT_SECRET_KEY);
+      req.user = jwtPayload;
+
+      return proceed();
+    } catch (error) {
+      return res.unauthorized();
+    }
+  
+};
