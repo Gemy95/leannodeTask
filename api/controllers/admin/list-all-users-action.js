a60eb1e9256968a51273d98d9abbade6f349cd78
Ali Gamal <ali.gamal@beyond-solution.com>
2022-09-06 00:30:39 +0200

    NF: handle pagination by admin on list all users api

--- api/controllers/admin/list-all-users-action.js
+++ api/controllers/admin/list-all-users-action.js
@@ -9,6 +9,9 @@ module.exports = {
   
     fn: async function () {
 
+        const page =  parseInt(this.req.query.page) || 0;
+        const limit = parseInt(this.req.query.limit) || 0;
+
         const users = (await User.get()).docs
         .map((doc) => {
           const user = { id: doc.id, ...doc.data() };
@@ -16,8 +19,18 @@ module.exports = {
           return user;
         });
 
+        let totalPages;
+        
+        if(page>0 && limit>0){
+            totalPages = Math.ceil(users.length / limit);
+        }
+
         return {
-            users
+            users: (page>0 & limit>0) ? users.slice((page-1)*limit, (limit * page)) : users ,
+            pagination:{
+                page,
+                totalPages
+            }
         }
         
       }
